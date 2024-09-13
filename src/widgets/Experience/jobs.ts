1b724ba122e4e1e14afe5f7a8a0f676becbbc4ec
alexhetley6107 <hetleyofficial@gmail.com>
2024-09-13 16:54:48 +0300

    fix: jobs

--- src/widgets/Experience/jobs.ts
+++ src/widgets/Experience/jobs.ts
@@ -7,18 +7,18 @@ export type JobType = {
 };
 
 export const jobs: JobType[] = [
-  {
-    id: 4,
-    title: 'Your Next Agency',
-    date: 'May 2024 - Present',
-    description:
-      "Development and support of the frontend of the company's internal tools, automation of development processes.",
-    skills: ['react', 'ts', 'nodejs', 'vite', 'vue', 'storybook', 'git', 'tailwind', 'swagger', 'graphql', 'eslint'],
-  },
+  // {
+  //   id: 4,
+  //   title: 'Your Next Agency',
+  //   date: 'May 2024 - Present',
+  //   description:
+  //     "Development and support of the frontend of the company's internal tools, automation of development processes.",
+  //   skills: ['react', 'ts', 'nodejs', 'vite', 'vue', 'storybook', 'git', 'tailwind', 'swagger', 'graphql', 'eslint'],
+  // },
   {
     id: 3,
     title: 'Siema',
-    date: 'September 2022 - April 2024',
+    date: 'September 2022 - August 2024',
     description:
       'Development of the frontend part of projects in the field of e-commerce and bigtech. Creating big system of React components. Writing autotests for interface components.',
     skills: ['react', 'redux', 'vite', 'git', 'csharp', 'nodejs', 'eslint', 'swagger', 'nextjs', 'figma'],
